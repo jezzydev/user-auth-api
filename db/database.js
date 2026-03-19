f5b641298d8a8ec15b74d12df0b1eab90b3e31a0
jezzydev <jezuy87@gmail.com>
2026-03-19 11:37:52 +0800

    Use connectionString to connect to db

--- db/database.js
+++ db/database.js
@@ -2,11 +2,12 @@ import pg from "pg";
 
 const { Pool } = pg;
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
 });
 
 pool.on("connect", () => {
